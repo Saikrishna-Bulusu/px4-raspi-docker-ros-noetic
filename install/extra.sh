574b1f142afd3ded5731580ebb6a765b281184f4
Saikrishna-Bulusu <142135673+Saikrishna-Bulusu@users.noreply.github.com>
2024-10-20 18:18:38 +0530

    humble to noetic

--- install/extra.sh
+++ install/extra.sh
@@ -17,8 +17,8 @@ sudo DEBIAN_FRONTEND=noninteractive  apt-get install --no-install-recommends -y 
 	qt5dxcb-plugin \
 	terminator \
 	vim \
-	ros-humble-mavros \
-    ros-humble-mavros-extras
+	ros-noetic-mavros \
+    ros-noetic-mavros-extras
 
 	
 
