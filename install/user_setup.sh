ffedab742cb4b9bb9efc12425e45c13e581c23aa
Saikrishna-Bulusu <142135673+Saikrishna-Bulusu@users.noreply.github.com>
2024-10-20 18:19:54 +0530

    humble to noetic

--- install/user_setup.sh
+++ install/user_setup.sh
@@ -7,7 +7,8 @@ rosdep update
 
 cat << EOF >> ~/.bashrc
 echo sourcing ~/.bashrc
-source /opt/ros/humble/setup.bash
+
+echo "source /opt/ros/noetic/setup.bash" 
 export CCACHE_TEMPDIR=/tmp/ccache
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export PYTHONWARNINGS=ignore:::setuptools.installer,ignore:::setuptools.command.install
@@ -15,8 +16,8 @@ if [ -f ~/work/gazebo/install/setup.sh ]; then
   source ~/work/gazebo/install/setup.sh
   echo "gazebo built, sourcing"
 fi
-if [ -f ~/work/ros2_ws/install/setup.sh ]; then
-  source ~/work/ros2_ws/install/setup.sh
+if [ -f ~/work/ros_ws/install/setup.sh ]; then
+  source ~/work/ros_ws/install/setup.sh
   echo "workspace built, sourcing"
 fi
 source /usr/share/colcon_cd/function/colcon_cd.sh
